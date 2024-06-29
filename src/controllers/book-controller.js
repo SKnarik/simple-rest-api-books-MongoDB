3031f80279edaece2f69769e2c7a8184f5fbe4d3
KnarikSoghoyan <knariksoghoyan@gmail.com>
2024-06-30 02:07:14 +0400

    Add updateBook functionality

--- src/controllers/book-controller.js
+++ src/controllers/book-controller.js
@@ -4,61 +4,57 @@ import { connection } from "../storages/db.js";
 import { ObjectId } from "mongodb";
 
 const db = await connection();
-const booksCollection = db.collection('books');
-
+const booksCollection = db.collection("books");
 
 export const createBook = async (req, res) => {
+  try {
+    const { title, author, genre, year_published, rating, summary } = req.body;
+    const createdAt = moment().format("MMMM Do YYYY, h:mm:ss a");
 
-      try {
-        const { title, author, genre, year_published, rating, summary } = req.body;
-        const createdAt = moment().format('MMMM Do YYYY, h:mm:ss a');
-    
-        const { error } = bookValidationSchema.validate(req.body);
-    
-        if (error) {
-          res.status(403).send(error.message);
-          return
-        }
-    
-        const newBook = {
-          title,
-          author,
-          genre,
-          year_published,
-          rating,
-          summary,
-          createdAt: createdAt,
-        };         
-    
-        const isExisting = await booksCollection.findOne({ title });
-        if (isExisting) {
-          res.status(400).send("Book already exists.");
-          return;
-        }
+    const { error } = bookValidationSchema.validate(req.body);
 
-        const books = await booksCollection.insertOne(newBook); 
-    
-        res.status(200).send({ book: newBook });
-      } catch (error) {
-        res.status(400).send({ error: error.message });
-      }
+    if (error) {
+      res.status(403).send(error.message);
+      return;
+    }
+
+    const newBook = {
+      title,
+      author,
+      genre,
+      year_published,
+      rating,
+      summary,
+      createdAt: createdAt,
+    };
+
+    const isExisting = await booksCollection.findOne({ title });
+    if (isExisting) {
+      res.status(400).send("Book already exists.");
+      return;
+    }
+
+    const books = await booksCollection.insertOne(newBook);
+
+    res.status(200).send({ book: newBook });
+  } catch (error) {
+    res.status(400).send({ error: error.message });
+  }
 };
 
 export const getBooks = async (req, res) => {
-
-    try {
-        const books = await booksCollection.find().toArray(); 
-        res.status(200).send({ books });
-    } catch (error) {
-      res.status(400).send({ error: error.message });
-    }
+  try {
+    const books = await booksCollection.find().toArray();
+    res.status(200).send({ books });
+  } catch (error) {
+    res.status(400).send({ error: error.message });
+  }
 };
 
 export const getBook = async (req, res) => {
-
   try {
     const { id } = req.params;
-    const thisBook = await booksCollection.findOne ({_id: new ObjectId(id)});  
+    const thisBook = await booksCollection.findOne({ _id: new ObjectId(id) });
 
     if (!thisBook) {
       throw new Error("Book not found");
@@ -70,10 +66,26 @@ export const getBook = async (req, res) => {
   }
 };
 
-export const updateBook = (req, res) => {
+export const updateBook = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const payload = req.body;
 
+    if (!id) {
+      throw new Error("ID parameter is required");
+    }
+
+    const updatedBook = await booksCollection.findOneAndUpdate(
+      { _id: new ObjectId(id) },
+      { $set: payload },
+      { returnOriginal: false }
+    );
+    if (!updatedBook) {
+      throw new Error("Book not found or not updated.");
+    }
+    res.status(200).send({ book: updatedBook });
+  } catch (error) {
+    res.status(404).send({ error: error.message });
+  }
 };
-
-export const deleteBook = (req, res) => {
-
-};
+export const deleteBook = (req, res) => {};
